98c0481e57eae9a33bbf08be41fe7f0ccbcd238b
katerinakiseliova <kiseliova1985@mail.ru>
2023-02-28 20:47:33 +0300

    Task_2

---
+++ Work_1/1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этиConsole.WriteLine("Введите первое число:");
+int n_1 = int.Parse(Console.ReadLine()!);
+int n_2 = int.Parse(Console.ReadLine()!);
+int n_3 = int.Parse(Console.ReadLine()!);
+
+if (n_1 > n_2)
+{
+    if (n_1 > n_3)
+    {
+Console.WriteLine("Максимальное число:" + n_1);
+    }
+    else
+    {
+Console.WriteLine("Максимальное число:" + n_3);
+    }
+}
+ else if (n_2 > n_3)
+{
+ Console.WriteLine("Максимальное число:" + n_2);   
+}
+else
+{
+Console.WriteLine("Максимальное число:" + n_3);    
+}
