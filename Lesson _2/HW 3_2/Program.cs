f47b342020ecf346e5e90aa64c4947fb0e89f16c
katerinakiseliova <kiseliova1985@mail.ru>
2023-03-06 09:47:47 +0300

    HW_2

---
+++ Lesson _2/HW 3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+Console.WriteLine("Введите чило");
+
+void ThirdNum(int num)
+{
+    Console.WriteLine($"{num} ->");
+    if (num < 100)
+    {
+        Console.WriteLine("there is no third digit");
+        return;
+    }
+
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()!));
+
+
+
+
+
---
+++ Lesson _2/Lesson2.1/3_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1: Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую wbahe цифру этого числа
+Console.WriteLine("Введите чило");
+string NumSec(int num)
+{
+    if((-1000 < num && num < -100) || (num < 1000 && num >100))
+      return $"{num / 10 % 10}";
+    return "The number is not three-digit!";
+}
+Console. WriteLine(NumSec(int.Parse(Console.ReadLine()!)));
